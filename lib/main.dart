1ba35d0c6d7a0a6ff1b7255965d38a9f5d234b27
fasihDL <fasihdeveloplogix@gmail.com>
2024-07-22 20:14:10 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:skhickens_app/bindings.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 

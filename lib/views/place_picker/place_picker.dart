4c3e34fc723817709a4fe125d328b4ae33b874ff
Mirza Asjad Basharat <asjad.developlogix@gmail.com>
2024-09-19 18:26:52 +0500

    unfocus the textfield by clicking anywhere on screen.

--- lib/views/place_picker/place_picker.dart
+++ lib/views/place_picker/place_picker.dart
@@ -2,7 +2,6 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
--- lib/widgets/auth_textfield.dart
+++ lib/widgets/auth_textfield.dart
@@ -19,75 +19,91 @@ class TextFieldWidget extends StatelessWidget {
   final Function()? onTap;
   List<TextInputFormatter>? inputFormatters;
   final TextInputType? keyboardType;
-  TextFieldWidget({super.key,
-    required this.text,this.path, required this.textController,
-    this.onSubmit,this.onChanged,
-    this.isPassword = false,this.onEditComplete, this.focusNode, this.keyboardType,
+
+  TextFieldWidget({
+    super.key,
+    required this.text,
+    this.path,
+    required this.textController,
+    this.onSubmit,
+    this.onChanged,
+    this.isPassword = false,
+    this.onEditComplete,
+    this.focusNode,
+    this.keyboardType,
     this.prefixIcon,
-     this.inputFormatters,
+    this.inputFormatters,
     this.isReadOnly = false,
     this.onTap,
   });
 
+  void _unfocus(BuildContext context) {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-              height: 7.h,
-              width: 100.w,
-              padding: EdgeInsets.only(left: 2.w,right: 1.w,),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: AppColors.whiteColor,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 6,
-                    offset: const Offset(0, 3)
-                  )
-                ]
-              ),
-              child: TextFormField(
-                readOnly: isReadOnly,
-                obscureText: isPassword,
-                controller: textController,
-                onEditingComplete: onEditComplete,
-                focusNode: focusNode,
-                keyboardType: keyboardType,
-                onFieldSubmitted: onSubmit,
-                onChanged: onChanged,
-                onTap: onTap,
-                inputFormatters: inputFormatters,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(2.h),
-                  prefixIcon: prefixIcon,
-                  hintText: text,
-                  hintStyle: poppinsRegular(fontSize: 13,color: const Color(0xFF858585)),
-                  border: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-
-                  suffixIcon: path.isEmptyOrNull ? null : Padding(
+    return GestureDetector(
+      onTap: () => _unfocus(context),
+      child: Container(
+        height: 7.h,
+        width: 100.w,
+        padding: EdgeInsets.only(left: 2.w, right: 1.w),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: AppColors.whiteColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 6,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: TextFormField(
+          readOnly: isReadOnly,
+          obscureText: isPassword,
+          controller: textController,
+          onEditingComplete: onEditComplete,
+          focusNode: focusNode,
+          keyboardType: keyboardType,
+          onFieldSubmitted: onSubmit,
+          onChanged: onChanged,
+          onTap: onTap,
+          inputFormatters: inputFormatters,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(2.h),
+            prefixIcon: prefixIcon,
+            hintText: text,
+            hintStyle:
+                poppinsRegular(fontSize: 13, color: const Color(0xFF858585)),
+            border: InputBorder.none,
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            suffixIcon: path.isEmptyOrNull
+                ? null
+                : Padding(
                     padding: const EdgeInsets.all(2),
                     child: Container(
                       height: 38.sp,
                       width: 38.sp,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: AppColors.whiteColor,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black.withOpacity(0.2),
-                                blurRadius: 6,
-                                offset: const Offset(0, 3)
-                            )
-                          ]
+                        borderRadius: BorderRadius.circular(50),
+                        color: AppColors.whiteColor,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 6,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
                       ),
-                      child: Image.asset(path!, scale: 3,),
+                      child: Image.asset(path!, scale: 3),
                     ),
-                  )
-                ),
-
-              ),
-            );
+                  ),
+          ),
+        ),
+      ),
+    );
   }
 }

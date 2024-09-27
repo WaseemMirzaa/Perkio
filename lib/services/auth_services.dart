712ef482b85c4ceff45a5eb387ea3c449dea23eb
Mirza Asjad Basharat <asjad.developlogix@gmail.com>
2024-09-27 15:37:27 +0500

    Update auth_services.dart

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -3,7 +3,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nb_utils/nb_utils.dart';
-import 'package:swipe_app/core/utils/constants/app_const.dart';
 import 'package:swipe_app/core/utils/constants/constants.dart';
 import 'package:swipe_app/models/user_model.dart';
 

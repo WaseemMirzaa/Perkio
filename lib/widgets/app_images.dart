9e1f3786dac8614179041093357c189c02d9d02b
Sohaib <159741509+Unicorndev021@users.noreply.github.com>
2025-05-14 21:00:33 +0500

    add logo with white background update customize slide button so in splash screen if background red then this button should be white and app logo should be red and in login when we have white background then button color should be red and logo should be white

--- lib/widgets/app_images.dart
+++ lib/widgets/app_images.dart
@@ -1,11 +1,8 @@
 class AppImages {
   AppImages._();
 
-
   static const String logo = 'assets/images/finalL.png';
   static const String logoBack = 'assets/images/final.jpeg';
   static const String logoRed = 'assets/images/final.jpeg';
-
-
-
+  static const String logoWhite = 'assets/images/finalLogoB.jpg';
 }
